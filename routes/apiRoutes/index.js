ac9624fa196db3d21e370f774c9920cb44a7b5a9
ckavanattu <chackojk@gmail.com>
2022-08-24 16:27:33 -0700

    added notes.js to push notes to db.json

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { notes } = require("../../db/db.json");
+const { createNote } = require("../../lib/notes");
 
 
 
@@ -9,7 +10,9 @@ router.get("/notes", (req, res) => {
 });
 
 router.post("/notes", (req, res) => {
-  req.body.id = // CORRECT THIS
+  req.body.id = notes.length.toString();
   const newNote = createNote(req.body, notes);
   res.json(newNote);
 });
+
+module.exports = router;
