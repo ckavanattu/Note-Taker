5a06151e9c9e57bb1253fe43043271fad33a9726
ckavanattu <chackojk@gmail.com>
2022-08-26 17:21:42 -0700

    .

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.static("public"));
-
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Listening to PORT ${PORT}`);
+  console.log(`API server now on port ${PORT}!`);
 });
